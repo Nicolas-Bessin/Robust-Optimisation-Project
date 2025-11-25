b16c9e17d3f3b4e7aa993410ea2ec21f89bb6739
Nicolas-Bessin <nicolasbessin49@gmail.com>
2025-11-25 15:31:13 +0100

    Julia TP + data for project

--- tp_julia/callback.jl
+++ tp_julia/callback.jl
@@ -1,81 +1,107 @@
 using JuMP
-using CPLEX
+using Gurobi, GLPK
+import Test
+import MathOptInterface as MOI
+
 
 function callbackMain()
+    println("------------------------------")
+    println("------------------------------")
     
-    m = Model(CPLEX.Optimizer)
+    optimizer = optimizer_with_attributes(
+           Gurobi.Optimizer, "Threads" => 1, "Presolve" => 0, "Method" => 1
+       );
+
+    m = Model(optimizer)
 
     # Il est imposé d'utiliser 1 seul thread en Julia avec CPLEX pour
     # utiliser les callbacks
-    MOI.set(m, MOI.NumberOfThreads(), 1)
+    # MOI.set(m, Gurobi.Threads(), 1)
+    # set_attribute(m, "Threads", 1)
+    # set_attribute(m, "Presolve", 0)
+    # set_attribute(m, "Method", 3)
+    # set_silent(m)
 
     @variable(m, x, Int)
     @constraint(m, x <= 10)
     @objective(m, Max, x)
 
-    # Fonction qui sera exécutée par CPLEX à chaque fois qu'une des conditions
-    # suivantes est remplie :
-    # - 1 solution entière a été trouvée ;
-    # - 1 relaxation a été calculée ;
-    # - ...
-    #
-    # Arguments :
-    # - context_id : permet de déterminer pour quelle raison le callback à
-    # été appelé ;
-    # - cb_data  :   permet  d'obtenir   d'autres   informations
-    #   (valeur des bornes inférieures et supérieures , meilleure solution
-    #   connue, ...)
-    function mon_super_callback(cb_data::CPLEX.CallbackContext, context_id::Clong)
+    lazy_cut_was_called = false
+    calls = 0
 
-        # On teste d'abord si c'est l'obtention d'une solution entière
-        # qui a entraîné l'appel du callback
-        # (cette fonction isIntegerPoint est définie ci-dessous mais son 
-        # contenu n'est pas très important)
-        if isIntegerPoint(cb_data, context_id)
-            
-            # Cette ligne doit être  appelée avant de pouvoir récupérer la
-            # solution entière ayant entraîné l'appel du callback
-            CPLEX.load_callback_variable_primal(cb_data, context_id)
+    function lazy_cut_generic(cb_data)
+        calls += 1
+        lazy_cut_was_called = true
 
-            # On récupère la valeur de x
-            x_val = callback_value(cb_data, x)
-
-            # Si elle est plus grande que 1, on ajoute la contrainte x <= 1
-            if x_val >= 2 - 1e-5
-                cstr = @build_constraint(x <= 1)
-                MOI.submit(m, MOI.LazyConstraint(cb_data), cstr)
-                println("Add constraint x <= 1")
-            end
+        status = callback_node_status(cb_data, m)
+        if status == MOI.CALLBACK_NODE_STATUS_FRACTIONAL
+            # `callback_value(cb_data, x)` is not integer (to some tolerance).
+            # If, for example, your lazy constraint generator requires an
+            # integer-feasible primal solution, you can add a `return` here.
+            return
+        elseif status == MOI.CALLBACK_NODE_STATUS_INTEGER
+            # `callback_value(cb_data, x)` is integer (to some tolerance).
+        else
+            @assert status == MOI.CALLBACK_NODE_STATUS_UNKNOWN
+            # `callback_value(cb_data, x)` might be fractional or integer.
         end
+        x_val = callback_value(cb_data, x)
+        println("Callback was called with x = $x_val")
+        if x_val > 1+ 1e-6
+            con = @build_constraint(x <= 1)
+            println("Adding constraint $con")
+            MOI.submit(m, MOI.LazyConstraint(cb_data), con)
+        end
+        return
     end
 
-    # On précise que le modèle doit utiliser notre fonction de callback
-    MOI.set(m, CPLEX.CallbackFunction(), mon_super_callback)
+    # On précise que le modèle doit utiliser notre fonction de callback 
+    set_attribute(m, MOI.LazyConstraintCallback(), lazy_cut_generic)
+
+
+    # function lazy_cut_gurobi(cb_data, cb_where :: Cint)
+    #     calls += 1
+    #     lazy_cut_was_called = true
+
+    #     if cb_where != GRB_CB_MIPSOL
+    #         return
+    #     end
+
+    #     # You can query a callback attribute using GRBcbget
+    #     if cb_where == GRB_CB_MIPNODE
+    #         resultP = Ref{Cint}()
+    #         GRBcbget(cb_data, cb_where, GRB_CB_MIPNODE_STATUS, resultP)
+    #         if resultP[] != GRB_OPTIMAL
+    #             return  # Solution is something other than optimal.
+    #         end
+    #     end
+
+    #     # Before querying `callback_value`, you must call:
+    #     Gurobi.load_callback_variable_primal(cb_data, cb_where)
+    #     x_val = callback_value(cb_data, x)
+
+    #     if x_val > 1 + 1e-6
+    #         con = @build_constraint(x <= 1)
+    #         println("Currently, x = $x_val, Adding a constraint : $con")
+    #         MOI.submit(m, MOI.LazyConstraint(cb_data), con)
+    #     end
+    #     return
+    # end
+
+    # # Solver dependent callback
+    # MOI.set(m, MOI.RawOptimizerAttribute("LazyConstraints"), 1)
+    # MOI.set(m, Gurobi.CallbackFunction(), lazy_cut_gurobi)
+
     optimize!(m)
+
+    # Prints
+    assert_is_solved_and_feasible(m)
+    Test.@test lazy_cut_was_called
+    Test.@test value(x) <= 1
+    println("Optimal solution x = $(value(x))")
+    println("Number of callback calls = $calls")
+    return
+
 end 
 
-# Fonction  permettant  de  déterminer  si  c'est  l'obtention  d'une
-# solution entière qui a entraîné l'appel d'un callback
-# (il n'est pas nécessaire d'en comprendre le fonctionnement)
-function isIntegerPoint(cb_data::CPLEX.CallbackContext, context_id::Clong)
-
-    # context_id  == CPX_CALLBACKCONTEXT_CANDIDATE si le  callback est
-    # appelé dans un des deux cas suivants :
-    # cas 1 - une solution entière a été obtenue; ou
-    # cas 2 - une relaxation non bornée a été obtenue
-    if context_id != CPX_CALLBACKCONTEXT_CANDIDATE
-        return false
-    end
-
-    # Pour déterminer si on est dans le cas 1 ou 2, on essaie de récupérer la
-    # solution entière courante
-    ispoint_p = Ref{Cint}()
-    ret = CPXcallbackcandidateispoint(cb_data, ispoint_p)
-
-    # S'il n'y a pas de solution entière
-    if ret != 0 || ispoint_p[] == 0
-        return false
-    else
-        return true
-    end
-end
+callbackMain()
