7dbbe506ecbfd809e38c3557c958430488f3e9ac
Nicolas-Bessin <nicolasbessin49@gmail.com>
2025-11-26 19:30:10 +0100

    Script to compile raw results with arbitrary criteria

--- src/results_manager.jl
+++ src/results_manager.jl
@@ -1,4 +1,4 @@
-using JSON, LibGit2, Dates
+using JSON, LibGit2, Dates, CSV, DataFrames
 
 struct SolutionInfo
     instance :: String
@@ -21,7 +21,7 @@ function write_solution_info_to_raw_file(
 
     raw_results_file = "$instance_res_dir/$(sol.method).csv"
     if !isfile(raw_results_file)
-        line = "instance, method, gap, time, status, cost, date, commit-id \n"
+        line = "instance,method,gap,time,status,cost,date,commit-id\n"
         write(raw_results_file, line)
     end
 
@@ -29,10 +29,76 @@ function write_solution_info_to_raw_file(
     commit_id = read(cmd, String)
     date = Dates.format(now(), "yyyy-mm-dd HH:MM:SS")
 
-    newline = "$(sol.instance), $(sol.method), $(sol.gap), $(sol.time), $(sol.status), $(sol.cost), $date, $commit_id"
+    newline = "$(sol.instance),$(sol.method),$(sol.gap),$(sol.time),$(sol.status),$(sol.cost),$date,$commit_id"
 
     open(raw_results_file, "a") do f
         write(f, newline)
     end
     return
+end
+
+function compile_raw_results(
+    raw_results_folder :: String,
+    output_res_file :: String,
+    criteria :: Vector{Symbol};
+    descending :: Bool = false,
+    instance_folder :: String = "data/"
+)
+    
+    output = Dict()
+
+    # Build the output by collecting the best result along the given criterion
+    for instance in readdir(instance_folder)
+        if instance == "solution.txt"
+            continue
+        end
+
+        instance_raw_results = "$raw_results_folder/$instance"
+        if !isdir(instance_raw_results)
+            println("No results for instance $instance, skipping")
+            continue
+        end
+        
+        # Needed only in a future version where we are not creating the output from scratch
+        if instance ∉ keys(output)
+            output[instance] = Dict()
+        end
+
+        # Process every method
+        for method_file in readdir(instance_raw_results)
+            method = splitext(method_file)[1]
+            filepath = "$instance_raw_results/$method_file"
+            method_data = CSV.read(filepath, DataFrame, header = true)
+            sort!(method_data, criteria, rev = descending)
+            # We want to keep the best method according to these criteria
+            best_data = method_data[1, :]
+            method_output = Dict()
+            method_output["time"] = best_data[:time]
+            method_output["gap"] = best_data[:gap]
+            method_output["status"] = best_data[:status]
+            method_output["cost"] = best_data[:cost]
+            method_output["date"] = best_data[:date]
+            method_output["commit-id"] = best_data[Symbol("commit-id")]
+
+            output[instance][method] = method_output
+        end
+
+    end
+
+    open(output_res_file, "w") do f
+        content = JSON.json(output, pretty = true)
+        write(f, content)
+    end
+
+    return
+end
+
+RUN_SCRIPTING = false
+if RUN_SCRIPTING
+    compile_raw_results(
+        "results/raw/",
+        "results/test.json",
+        [Symbol("time")],
+        descending = false
+    )
 end
